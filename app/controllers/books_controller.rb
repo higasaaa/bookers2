0445f6a880b473b5ec3753451f9f7d438020da75
higasaaa <higasaaa26@gmail.com>
2022-12-25 17:16:47 +0000

    book_edit user_edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,8 +27,8 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.new
-    @books = Book.all
+    @book = Book.find(params[:id])
+
     @user = current_user
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
     has_one_attached :image
     
-    validates :title, presence: true
-    validates :body, presence: true
-
+    validates :title, presence: true 
+    validates :body, presence: true, length: { maximum: 200 }
+    
+    # validates :introduction, length: { maximum: 50 }
 
     # ユーザーに属する（userに関連付ける記述）
     # has_many とは逆に、1:N の「N」側にあたるモデルに、belongs_to を記載する必要がある
