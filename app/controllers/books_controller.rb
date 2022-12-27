03053518f723cd46bb059ca781e824d3a61ff72c
higasaaa <higasaaa26@gmail.com>
2022-12-27 07:12:47 +0000

    [Finish2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,8 +42,6 @@ class BooksController < ApplicationController
   end
 
   def update
-    # @books = Book.all
-    # @user = current_user
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
